1f07c1d25ec09ecd9e98876f02a6cbdc132810cf
Antanas Bastys <antanas.bastys@tieto.com>
2016-09-20 12:45:32 +0300

    Initial spring project

---
+++ src/main/java/lt/akademija/javatech/JavatechApplication.java
@@ -0,0 +1,12 @@
+package lt.akademija.javatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatechApplication.class, args);
+	}
+}
